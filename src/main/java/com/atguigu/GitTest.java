a4bf84d35f356038ddaabc285b30d1ffb0506ed2
Leo <Leo@12306.com>
2022-03-29 11:50:19 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,17 @@
+package com.atguigu;
+
+/**
+ * @author chpstart
+ * @create 2022-03-29 10:46
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git1!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+        System.out.println("master");
+
+        System.out.println("hot-fix");
+    }
+}
